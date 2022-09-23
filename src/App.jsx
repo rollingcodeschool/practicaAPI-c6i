bb2646ef4ad4956e322210b0fcdb8fa6532e7509
Tomas <tomibudeguer20@gmail.com>
2022-09-22 23:21:00 -0300

    hice el componente App junto con el css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container} from "react-bootstrap";
+import './style.css'
 
 function App() {
   return (
-    <h1 className="display-1">Prueba</h1>
+    <Container>
+      <div className="m-5">
+      <img
+        src="https://trello.com/1/cards/632a448eb47fe800d9bdef25/attachments/632cd857ffabe0042d12baf2/download/image.png"
+        alt="the simpsons"
+      />
+      </div>
+      <button className="botonNaranja">Obtener frase</button>
+    </Container>
   );
 }
 
